8e5c7409ddbabff02870f2806031da5876ce1a5c
alexb02h <70178849+alexb02h@users.noreply.github.com>
2023-02-12 20:27:36 -0800

    Delete .Module.symvers.cmd

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/alexander/src/Module.symvers := sed 's/\.ko$$/\.o/' /home/alexander/src/modules.order | scripts/mod/modpost -m -a  -o /home/alexander/src/Module.symvers -e -i Module.symvers   -T -
